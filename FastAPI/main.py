f8da6d9a7e240de7c731b58f8ed6067863e03c3a
allankimanzi2 <allankim457@gmail.com>
2024-03-25 01:43:48 +0300

    added dependencies to main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,94 +1,84 @@
 # main.py
 import uvicorn
 import pyrebase
-from fastapi import FastAPI
-from models import LoginSchema,SignUpSchema
+from fastapi import FastAPI, HTTPException
+from models import LoginSchema, SignUpSchema
 from fastapi.responses import JSONResponse
-from fastapi.exceptions import HTTPException
 from fastapi.requests import Request
+from firebase_admin import credentials, auth, initialize_app
 
 app = FastAPI()
 
-
-import firebase_admin
-from firebase_admin import credentials,auth
-
 if not firebase_admin._apps:
     cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred)
+    initialize_app(cred)
 
 firebaseConfig = {
-
-  "apiKey": "AIzaSyA4jJ_OW8wOxjD36yr7FPiTNAmavmLGYbI",
-  "authDomain": "admin-app-71c16.firebaseapp.com",
-  "projectId": "admin-app-71c16",
-  "storageBucket": "admin-app-71c16.appspot.com",
-  "messagingSenderId": "483305910482",
-  "appId": "1:483305910482:web:0a00e66f316cee45afbd0b",
-  "measurementId": "G-9THRNVXY2S"
+    "apiKey": "AIzaSyA4jJ_OW8wOxjD36yr7FPiTNAmavmLGYbI",
+    "authDomain": "admin-app-71c16.firebaseapp.com",
+    "projectId": "admin-app-71c16",
+    "storageBucket": "admin-app-71c16.appspot.com",
+    "messagingSenderId": "483305910482",
+    "appId": "1:483305910482:web:0a00e66f316cee45afbd0b",
+    "measurementId": "G-9THRNVXY2S"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
-
-
 
 @app.get("/")
 async def root():
     return {"message": ""}
 
 @app.post('/signup')
-async def create_an_account(user_data:SignUpSchema):
+async def create_an_account(user_data: SignUpSchema):
     email = user_data.email
     password = user_data.password
 
     try:
         user = auth.create_user(
-        email = email,
-        password = password
+            email=email,
+            password=password
         )
 
-        return JSONResponse(content={"message" : f"User account created successfully for user {user.uid}"},
-                            status_code = 201
+        return JSONResponse(content={"message": f"User account created successfully for user {user.uid}"},
+                            status_code=201
                            )
     except auth.EmailAlreadyExistsError:
         raise HTTPException(
            status_code=400,
-           detail= f"Account already created for the email {email}"
+           detail=f"Account already created for the email {email}"
         )
+
 @app.post('/login')
-async def create_access_token(user_data:LoginSchema):
+async def create_access_token(user_data: LoginSchema):
     email = user_data.email
     password = user_data.password
 
     try:
-        user = firebaseauth().sign_in_with_email_and_password(
-        email = email,
-        password = password
+        user = auth.sign_in_with_email_and_password(
+            email=email,
+            password=password
         )
+        token = user['idToken']
 
-    token = user['idToken']
-
-    return JSONResponse(
-        content={
-            "token":token
-        },status_code=200
-    )
+        return JSONResponse(
+            content={"token": token},
+            status_code=200
+        )
 
     except:
         raise HTTPException(
-            status_code=400,detail="Invalid Credentials"
+            status_code=400, detail="Invalid Credentials"
         )
 
 @app.post('/ping')
-async def validate_token(request:Request):
+async def validate_token(request: Request):
     headers = request.headers
     jwt = headers.get('authorization')
 
     user = auth.verify_id_token(jwt)
 
     return user.uid
-
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
